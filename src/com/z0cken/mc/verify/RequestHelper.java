e707fa93c007c83314e594c95b0a14dd7c0ec0e2
Flare <flarefps@gmail.com>
2019-01-20 22:45:02 +0100

    stuff

---
+++ src/com/z0cken/mc/verify/RequestHelper.java
@@ -0,0 +1,59 @@
+package com.pr0gramm.mc.verify;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import net.md_5.bungee.BungeeCord;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+public class RequestHelper {
+
+    static long lastCheck = PCS_Checkpoint.getInstance().config.getLong("lastCheck");
+    private static final String COOKIE = PCS_Checkpoint.getInstance().config.getString("cookie");
+
+    public static boolean isBanned(String name) throws UnirestException {
+
+        String request = "https://pr0gramm.com/api/profile/info?name=" + name;
+        JsonNode jsonBody = Unirest.get(request).header("accept", "application/json").asJson().getBody();
+        JSONObject json = jsonBody.getObject();
+
+        return 1 == json.getJSONObject("user").getInt("banned");
+    }
+
+    static void checkVerifications() {
+
+        long newTime = System.currentTimeMillis();
+
+        String url = "https://pr0gramm.com/api/inbox/messages";
+        HttpResponse<JsonNode> response;
+
+        try {
+            response = Unirest.get(url).header("accept", "application/json").header("cookie", COOKIE).asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        //TODO Check if UnirestException covers this
+        if(response.getStatus() != 200) {
+            BungeeCord.getInstance().getLogger().severe("Could not check for verifications (HTTP " + response.getStatus() + ")");
+            return;
+        }
+
+        JSONArray array = response.getBody().getObject().getJSONArray("messages");
+
+        for(Object o : array){
+            JSONObject obj = (JSONObject) o;
+
+            if(obj.getLong("created") > lastCheck) {
+                if(obj.getBoolean("sent")) continue;
+                DatabaseHelper.verify(obj.getString("message"), obj.getString("senderName"));
+            }
+        }
+        lastCheck = newTime;
+    }
+
+}
---
+++ src/com/z0cken/mc/verify/Util.java
@@ -0,0 +1,29 @@
+package com.pr0gramm.mc.verify;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
+
+import java.util.UUID;
+
+public class Util {
+
+    public static UUID getMojangUUID(String name) throws IllegalArgumentException, UnirestException {
+        String request = "https://api.mojang.com/users/profiles/minecraft/" + name;
+        HttpResponse<JsonNode> response = Unirest.get(request).header("accept", "application/json").asJson();
+
+        if(response.getStatus() != 200) throw new IllegalArgumentException("UUID for " + name + "not available (Status " + response.getStatus() + ")");
+
+        JSONObject json = response.getBody().getObject();
+        return UUID.fromString(insertDashUUID(json.getString("id")));
+    }
+
+    private static String insertDashUUID(String uuid) {
+        StringBuffer sb = new StringBuffer(uuid);
+        sb.insert(8, "-").insert(13, "-").insert(18, "-").insert(23, "-");
+
+        return sb.toString();
+    }
+}
