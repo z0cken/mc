453f64ba8176deb5a83a2592a058045819a93d4f
Flare <flarefps@gmail.com>
2019-02-26 14:40:39 +0100

    Dedicated config files for each module

--- src/main/java/com/z0cken/mc/essentials/modules/Module.java
+++ src/main/java/com/z0cken/mc/essentials/modules/Module.java
@@ -2,25 +2,29 @@ package com.z0cken.mc.essentials.modules;
 
 import com.z0cken.mc.essentials.PCS_Essentials;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @SuppressWarnings("WeakerAccess")
 public abstract class Module {
 
-    protected final Collection<BukkitTask> tasks = new ArrayList<>();
+    private final String NAME;
     private final Collection<String> commands = new ArrayList<>();
-    private final String configPath;
-    protected ConfigurationSection config;
+    protected final Collection<BukkitTask> tasks = new ArrayList<>();
 
-    Module(String configPath) {
-        this.configPath = configPath;
+    private YamlConfiguration config;
+
+    Module(String name) {
+        this.NAME = name;
         loadConfig();
         load();
 
@@ -30,27 +34,32 @@ public abstract class Module {
     }
 
     private void loadConfig() {
-        config = PCS_Essentials.getInstance().getConfig().getConfigurationSection(configPath);
+        PCS_Essentials.getInstance().saveResource(NAME + ".yml", false);
+        config = YamlConfiguration.loadConfiguration(new File(PCS_Essentials.getInstance().getDataFolder(), NAME + ".yml"));
     }
 
-    protected void registerCommand(String command) {
+    protected final void registerCommand(String command) {
         commands.add(command);
         if(this instanceof CommandExecutor) {
             PCS_Essentials.getInstance().getCommand(command).setExecutor((CommandExecutor) this);
         } else throw new RuntimeException("Could not register \"" + command + "\" - Module does not implement CommandExecutor");
     }
 
-    public void disable() {
+    public final void disable() {
         if(this instanceof Listener) HandlerList.unregisterAll((Listener) this);
         if(this instanceof CommandExecutor) commands.forEach(cmd -> PCS_Essentials.getInstance().getCommand(cmd).setExecutor(null));
         tasks.forEach(BukkitTask::cancel);
     }
 
-    public void reload() {
+    public final void reload() {
         loadConfig();
         load();
     }
 
     protected abstract void load();
+
+    protected final YamlConfiguration getConfig() {
+        return config;
+    }
 
 }
