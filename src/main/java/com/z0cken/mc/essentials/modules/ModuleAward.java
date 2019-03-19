751f5e34df06c053ea0ae60b0aa698ca14030937
Flare <flarefps@gmail.com>
2019-03-19 01:49:11 +0100

    Removed early supporter badge Updated to match Core version 1.1

--- src/main/java/com/z0cken/mc/essentials/modules/ModuleAward.java
+++ src/main/java/com/z0cken/mc/essentials/modules/ModuleAward.java
@@ -1,22 +1,15 @@
 package com.z0cken.mc.essentials.modules;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
 import com.z0cken.mc.core.persona.Persona;
 import com.z0cken.mc.core.persona.PersonaAPI;
-import org.apache.commons.lang.time.DateUtils;
-import org.apache.http.client.HttpResponseException;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.sql.SQLException;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class ModuleAward extends Module implements Listener, CommandExecutor {
 
@@ -30,29 +23,6 @@ public class ModuleAward extends Module implements Listener, CommandExecutor {
 
     }
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        Calendar calendar = new GregorianCalendar();
-        calendar.set(2019, Calendar.MARCH, 9);
-        if(DateUtils.isSameDay(Calendar.getInstance(), calendar) && !event.getPlayer().hasPermission("pcs.essentials.badge.bypass")) {
-            Persona persona = null;
-            try {
-                persona = PersonaAPI.getPersona(event.getPlayer().getUniqueId());
-            } catch (SQLException | UnirestException | HttpResponseException e) {
-                e.printStackTrace();
-            }
-
-            if(persona != null) {
-                try {
-                    persona.awardBadge(Persona.Badge.EARLY_SUPPORTER);
-                } catch (SQLException e) {
-                    getLogger().severe("Failed to award " + Persona.Badge.EARLY_SUPPORTER.name() + " badge to " + event.getPlayer().getName());
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
     @Override
     public boolean onCommand(CommandSender commandsender, Command command, String s, String[] args) {
         if(command.getName().equalsIgnoreCase("award")) {
@@ -60,20 +30,9 @@ public class ModuleAward extends Module implements Listener, CommandExecutor {
             if(target != null) {
                 try {
                     Persona.Badge badge = Persona.Badge.valueOf(args[1].toUpperCase());
-                    Persona persona = null;
 
-                    try {
-                        persona = PersonaAPI.getPersona(target.getUniqueId());
-                    } catch (UnirestException | HttpResponseException e) {
-                        e.printStackTrace();
-                    }
-
-                    if(persona != null) {
-                        persona.awardBadge(badge);
-                        commandsender.sendMessage("Badge freigeschaltet!");
-                    } else {
-                        commandsender.sendMessage("Persona nicht gefunden!");
-                    }
+                    PersonaAPI.getPersona(target.getUniqueId()).awardBadge(badge);
+                    commandsender.sendMessage("Badge freigeschaltet!");
 
                 } catch (IllegalArgumentException e) {
                     commandsender.sendMessage("Badge nicht gefunden!");
