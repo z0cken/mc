704275008d8ae1287cde2fef382d7d5094e4a6cd
Flare <flarefps@gmail.com>
2019-11-18 22:01:44 +0100

    1.14 update

--- src/main/java/com/z0cken/mc/metro/util/NMSUtil.java
+++ src/main/java/com/z0cken/mc/metro/util/NMSUtil.java
@@ -1,9 +1,9 @@
 package com.z0cken.mc.metro.util;
 
-import net.minecraft.server.v1_13_R2.DimensionManager;
-import net.minecraft.server.v1_13_R2.WorldMap;
+import net.minecraft.server.v1_14_R1.DimensionManager;
+import net.minecraft.server.v1_14_R1.WorldMap;
 import org.bukkit.World;
-import org.bukkit.craftbukkit.v1_13_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
 import org.bukkit.map.MapView;
 
 import java.util.Set;
@@ -14,11 +14,11 @@ public class NMSUtil {
     private NMSUtil() {}
 
     public static Set<MapView> getLoadedMaps(World world) {
-        return ((CraftWorld)world).getHandle().worldMaps.worldMap.get(DimensionManager.OVERWORLD).data.values().stream().filter(pb -> pb instanceof WorldMap).map(pb -> ((WorldMap)pb).mapView).collect(Collectors.toSet());
+        return ((CraftWorld)world).getHandle().getMinecraftServer().getWorldServer(DimensionManager.OVERWORLD).getWorldPersistentData().data.values().stream().filter(pb -> pb instanceof WorldMap).map(pb -> ((WorldMap)pb).mapView).collect(Collectors.toSet());
     }
 
     public static MapView getMapIfLoaded(World world, short id) {
-        WorldMap worldMap = (WorldMap) ((CraftWorld)world).getHandle().worldMaps.worldMap.get(DimensionManager.OVERWORLD).data.get("map_" + id);
+        WorldMap worldMap = (WorldMap) ((CraftWorld)world).getHandle().getMinecraftServer().getWorldServer(DimensionManager.OVERWORLD).getWorldPersistentData().data.get("map_" + id);
         return worldMap == null ? null : worldMap.mapView;
     }
 
