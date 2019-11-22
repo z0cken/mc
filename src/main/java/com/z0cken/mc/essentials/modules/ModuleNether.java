21a8fce580fcb82ebb7afa3a62f1a351ca833c4f
Flare <flarefps@gmail.com>
2019-11-22 18:42:33 +0100

    Removed spawner protection from Nether module Added centralized nether spawn Added nether->overworld portal toggle

--- src/main/java/com/z0cken/mc/essentials/modules/ModuleNether.java
+++ src/main/java/com/z0cken/mc/essentials/modules/ModuleNether.java
@@ -1,24 +1,48 @@
 package com.z0cken.mc.essentials.modules;
 
-import org.bukkit.GameMode;
-import org.bukkit.Material;
+import com.z0cken.mc.core.util.MessageBuilder;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class ModuleNether extends Module implements Listener {
     ModuleNether(String configPath) {
         super(configPath);
     }
 
+    private static boolean travelToOverworld;
+    private static BaseComponent[] denyMessage;
+    private static Location netherSpawn;
+
     @Override
     protected void load() {
-
+        travelToOverworld = getConfig().getBoolean("travel-to-overworld");
+        final String msg = getConfig().getString("messages.travel-disabled");
+        denyMessage = msg == null ? null : MessageBuilder.DEFAULT.build(msg);
+        netherSpawn = getConfig().getSerializable("nether-spawn", Location.class);
     }
 
-    @EventHandler
-    public void onBreak(BlockBreakEvent event) {
-        if(event.getBlock().getType() == Material.SPAWNER && event.getBlock().getWorld().getEnvironment() == World.Environment.NETHER && event.getPlayer().getGameMode() != GameMode.CREATIVE) event.setCancelled(true);
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onTeleport(PlayerTeleportEvent event) {
+        if(event.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
+            if(event.getTo().getWorld().getEnvironment() == World.Environment.NETHER) {
+                if(netherSpawn != null) {
+                    event.setTo(netherSpawn);
+                } else {
+                    event.getPlayer().spigot().sendMessage(denyMessage);
+                    event.setCancelled(true);
+                }
+            } else {
+                if(!travelToOverworld) {
+                    event.setCancelled(true);
+                    event.getPlayer().spigot().sendMessage(denyMessage);
+                }
+            }
+        }
+
     }
 }
