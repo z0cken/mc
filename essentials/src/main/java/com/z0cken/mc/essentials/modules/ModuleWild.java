3e9007a3d2bc67903ecb487ac1a68cde0b266c00
Flare <flarefps@gmail.com>
2021-02-21 17:11:00 +0100

    Add nether support for /wild

--- essentials/src/main/java/com/z0cken/mc/essentials/modules/ModuleWild.java
+++ essentials/src/main/java/com/z0cken/mc/essentials/modules/ModuleWild.java
@@ -1,9 +1,10 @@
 package com.z0cken.mc.essentials.modules;
 
-import com.z0cken.mc.claim.PCS_Claim;
 import com.z0cken.mc.core.util.MessageBuilder;
 import com.z0cken.mc.essentials.PCS_Essentials;
 import org.bukkit.*;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,33 +15,62 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
 
+import static org.bukkit.Material.*;
+
 public class ModuleWild extends Module implements CommandExecutor {
 
-    private int DISTANCE_SQUARED;
+    private int MIN_RAD_SQRD;
     private int COOLDOWN;
     private int LIMIT;
 
-    private Set<Material> VALID_MATERIALS = Set.of(
-            Material.GRASS_BLOCK,
-            Material.DIRT,
-            Material.STONE,
-            Material.GRAVEL,
-            Material.SAND,
-            Material.GRASS,
-            Material.CLAY,
-            Material.COARSE_DIRT,
-            Material.RED_SAND,
-            Material.TERRACOTTA,
-            Material.YELLOW_TERRACOTTA,
-            Material.SNOW,
-            Material.ICE,
-            Material.BLUE_ICE,
-            Material.FROSTED_ICE,
-            Material.PACKED_ICE,
-            Material.FERN
+    private final Set<Material> NORMAL_MATERIALS = EnumSet.of(
+            GRASS_BLOCK,
+            DIRT,
+            STONE,
+            GRAVEL,
+            SAND,
+            GRASS,
+            CLAY,
+            COARSE_DIRT,
+            RED_SAND,
+            TERRACOTTA,
+            YELLOW_TERRACOTTA,
+            SNOW,
+            ICE,
+            BLUE_ICE,
+            FROSTED_ICE,
+            PACKED_ICE
     );
 
-    private Map<UUID, Integer> players = new HashMap<>();
+    private final Set<Material> NETHER_MATERIALS = EnumSet.of(
+            NETHERRACK,
+            NETHER_QUARTZ_ORE,
+            BASALT,
+            SOUL_SAND,
+            SOUL_SOIL,
+            WARPED_NYLIUM,
+            WARPED_NYLIUM,
+            BLACKSTONE,
+            OBSIDIAN
+    );
+
+    private final Set<Material> IGNORED_MATERIALS = EnumSet.of(
+            AIR,
+            GRASS,
+            FERN,
+            ACACIA_LEAVES,
+            BIRCH_LEAVES,
+            DARK_OAK_LEAVES,
+            JUNGLE_LEAVES,
+            OAK_LEAVES,
+            SPRUCE_LEAVES,
+            BROWN_MUSHROOM,
+            RED_MUSHROOM,
+            CRIMSON_ROOTS,
+            NETHER_SPROUTS
+    );
+
+    private final Map<UUID, Integer> cooldowns = new HashMap<>();
 
     ModuleWild(String configPath) {
         super(configPath);
@@ -87,12 +117,7 @@ public class ModuleWild extends Module implements CommandExecutor {
 
             if(player != null) {
 
-                if(player.getWorld().getEnvironment() != World.Environment.NORMAL) {
-                    player.sendMessage(getConfig().getString("messages.error"));
-                    return true;
-                }
-
-                Integer remaining = players.getOrDefault(player.getUniqueId(), null);
+                Integer remaining = cooldowns.getOrDefault(player.getUniqueId(), null);
                 if(remaining == null) {
                     final Location target = getRandomLocation(player.getWorld(), LIMIT);
                     if(target == null) player.sendMessage(getConfig().getString("messages.error"));
@@ -100,7 +125,7 @@ public class ModuleWild extends Module implements CommandExecutor {
                     else {
                         player.spigot().sendMessage(MessageBuilder.DEFAULT.define("VALUE", Integer.toString((int) target.distance(player.getLocation()))).build(getConfig().getString("messages.success")));
                         player.teleport(target);
-                        if(!player.hasPermission("pcs.essentials.wild.bypass")) players.put(player.getUniqueId(), COOLDOWN);
+                        if(!player.hasPermission("pcs.essentials.wild.bypass")) cooldowns.put(player.getUniqueId(), COOLDOWN);
                     }
                 } else player.spigot().sendMessage(MessageBuilder.DEFAULT.define("VALUE", remaining.toString()).build(getConfig().getString("messages.wait")));
             }
@@ -111,13 +136,13 @@ public class ModuleWild extends Module implements CommandExecutor {
 
     @Override
     protected void load() {
-        DISTANCE_SQUARED = (int) Math.pow(getConfig().getDouble("min-distance"), 2.0D);
+        MIN_RAD_SQRD = (int) Math.pow(getConfig().getDouble("min-distance"), 2.0D);
         COOLDOWN = getConfig().getInt("cooldown");
         LIMIT = getConfig().getInt("limit");
 
         tasks.add(new BukkitRunnable(){
             @Override public void run() {
-                Iterator<Map.Entry<UUID, Integer>> iterator = players.entrySet().iterator();
+                Iterator<Map.Entry<UUID, Integer>> iterator = cooldowns.entrySet().iterator();
                 while (iterator.hasNext()) {
                     final Map.Entry<UUID, Integer> next = iterator.next();
                     next.setValue(next.getValue() - 1);
@@ -130,21 +155,73 @@ public class ModuleWild extends Module implements CommandExecutor {
     private Location getRandomLocation(World world, int limit) {
         if(limit <= 0) return null;
         Random r = new Random();
-        final WorldBorder worldBorder = world.getWorldBorder();
-        Location location = worldBorder.getCenter();
-        final int max = (int) worldBorder.getSize() / 2;
-        location.add(r.nextInt(max) - r.nextInt(max), 0, r.nextInt(max) - r.nextInt(max));
-        location.setY(world.getMaxHeight() - 1);
+        WorldBorder worldBorder = world.getWorldBorder();
+        int max = (int) worldBorder.getSize() / 2;
+        World.Environment environment = world.getEnvironment();
 
-        if(location.distanceSquared(worldBorder.getCenter()) < DISTANCE_SQUARED || location.getBlock().getBiome().name().endsWith("OCEAN")|| PCS_Claim.getClaim(location.getChunk()) != null) return getRandomLocation(world, --limit);
+        for (int i = 0; i < limit; i++) {
+            Location location = worldBorder.getCenter();
 
-        Material currentType = location.getBlock().getType();
-        while (currentType == Material.AIR || currentType.name().endsWith("LEAVES")) {
-            location.add(0, -1, 0);
-            currentType = location.getBlock().getType();
+            //Find random position beyond min distance that's not ocean
+            int tests = 0;
+            int x;
+            int z;
+            Biome biome;
+            do {
+                x = r.nextInt(max);
+                z = r.nextInt(max);
+                biome = location.clone().add(x, 0, z).getBlock().getBiome();
+            } while (tests++ < 100 && (x * x + z * z < MIN_RAD_SQRD || (environment == World.Environment.NORMAL && biome.name().endsWith("OCEAN"))));
+
+            location.add(x, 0, z);
+
+            //Vertical search
+            switch (environment) {
+                case NORMAL: {
+                    if(MIN_RAD_SQRD >= max * max * 2) return null; //Square circumscribed
+
+                    location.setY(world.getMaxHeight() - 1);
+                    Material currentType = location.getBlock().getType();
+                    while (IGNORED_MATERIALS.contains(currentType) && location.getY() > 0) {
+                        location.add(0, -1, 0);
+                        currentType = location.getBlock().getType();
+                    }
+
+                    final Block block = location.getBlock();
+                    if(NORMAL_MATERIALS.contains(block.getType()) && !block.getRelative(0, 2, 0).getType().isSolid()) return location.add(0, 1, 0);
+                    break;
+                }
+                case NETHER: {
+                    //Divide by 8^2
+                    if(MIN_RAD_SQRD >> 6 >= max * max * 2) return null; //Square circumscribed
+
+                    //Bottom up
+                    location.setY(32);
+                    Material currentType = location.getBlock().getType();
+                    boolean ground = false, feet = false;
+
+                    while (location.getY() < 128) {
+                        if(NETHER_MATERIALS.contains(currentType)) {
+                            ground = true;
+                            feet = false;
+                        } else if(ground && IGNORED_MATERIALS.contains(currentType)) {
+                            if(feet) {
+                                //Head clear
+                                return location.add(0, -1, 0);
+                            } else feet = true;
+                        }
+
+                        location.add(0, 1, 0);
+                        currentType = location.getBlock().getType();
+                    }
+                    break;
+                }
+                default: {
+                    return null;
+                }
+            }
         }
 
-        if(VALID_MATERIALS.contains(location.getBlock().getType()) && location.getBlock().getRelative(0, 2, 0).getType() == Material.AIR) return location.add(0, 1, 0);
-        else return getRandomLocation(world, --limit);
+        return null;
     }
 }
